10291678ae338822738cc0ec7100fe7a637bcb3a
xiaokai <1020849724@qq.com>
2022-12-21 01:37:45 +0800

    权限列表完成

--- src/components/home/roles/Rights.vue
+++ src/components/home/roles/Rights.vue
@@ -8,7 +8,26 @@
     </el-breadcrumb>
 
     <!-- 卡片部分 -->
-    <el-card> </el-card>
+    <el-card>
+      <!-- 表格部分 -->
+      <el-table :data="RoleDataList" border stripe style="width: 100%">
+        <el-table-column type="index" label="序号"> </el-table-column>
+        <el-table-column prop="authName" label="权限名称"> </el-table-column>
+        <el-table-column prop="path" label="权限路径"> </el-table-column>
+        <el-table-column prop="level" label="权限等级">
+          <template slot-scope="scope">
+            <!-- scope.row -->
+            <el-tag v-if="scope.row.level == 0">一级</el-tag>
+            <el-tag type="success" v-else-if="scope.row.level == 1">
+              二级
+            </el-tag>
+            <el-tag type="warning" v-else-if="scope.row.level == 2">
+              三级
+            </el-tag>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
   </div>
 </template>
 
@@ -16,13 +35,33 @@
 export default {
   name: "Rights",
   data() {
-    return {};
+    return {
+      // 权限列表数据
+      RoleDataList: [],
+    };
   },
   components: {},
   props: {},
-  methods: {},
+  methods: {
+    // 获取权限列表数据
+    async getRoleDataList() {
+      // 发送请求
+      let { data: res } = await this.$http.get(`/rights/list`);
+      if (res.meta.status == 200) {
+        // 设置权限列表数据
+        this.RoleDataList = res.data;
+        // console.log(this.RoleDataList);
+      } else {
+        this.$message.error("获取权限列表数据失败");
+      }
+    },
+  },
   computed: {},
   watch: {},
+  mounted() {
+    // 获取权限列表数据
+    this.getRoleDataList();
+  },
 };
 </script>
 
