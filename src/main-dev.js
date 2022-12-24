9f64d764d7ef1a08093db9503d8244e0dd83b8e2
xiaokai <1020849724@qq.com>
2022-12-25 03:31:39 +0800

    项目完成 优化完成 部署完成

---
+++ src/main-dev.js
@@ -0,0 +1,41 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+// 引入全局css样式
+import "@/assets/css/global.css";
+
+// 引入iconFont字体图标
+import "@/assets/fonts/iconfont.css";
+
+// 引入axios
+import axios from "@/axios/index.js";
+Vue.prototype.$http = axios;
+
+// 引入element UI
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+Vue.use(ElementUI);
+
+// 引入vue-table-with-tree-grid表格框架
+import TreeTable from "vue-table-with-tree-grid";
+Vue.component("tree-table", TreeTable);
+
+// 引入富文本编辑器
+import VueQuillEditor from "vue-quill-editor";
+// 引入富文本编辑器的样式
+import "quill/dist/quill.core.css";
+import "quill/dist/quill.snow.css";
+import "quill/dist/quill.bubble.css";
+Vue.use(VueQuillEditor);
+
+
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  publicPath: "./",
+  chainWebpack: (config) => {
+    // 判断是生产环境打包main-prod.js
+    config.when(process.env.NODE_ENV === "production", (config) => {
+      config.entry("app").clear().add("@/main-prod.js");
+
+      // // 生产环境加载外部CDN资源
+      // config.set("externals", {
+      //   vue: "Vue",
+      //   axios: "axios",
+      //   echarts: "echarts",
+      //   "vue-quill-editor": "VueQuillEditor",
+      //   lodash: "_",
+      // });
+    });
+    // 判断是开发环境打包main-dev.js
+    config.when(process.env.NODE_ENV === "development", (config) => {
+      config.entry("app").clear().add("@/main-dev.js");
+    });
+  },
+});
